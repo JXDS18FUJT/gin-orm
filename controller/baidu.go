c8f5b58f4edad3d60849158973ceb1bbed41fac0
18年后再见 <347613781@qq.com>
2021-04-20 14:59:12 +0800

    更新token

--- controller/baidu.go
+++ controller/baidu.go
@@ -20,7 +20,6 @@ type webimageRes struct {
 	Words_result_num int    `json:"words_result_num"`
 	Words_result     []Word `json:"words_result"`
 }
-
 func BaiduWebimage(c *gin.Context) {
 	var __dirname = "E:\\gin\\gin-orm\\txt\\"
 	// var accessToken = "24.2f5612a038f5c41bf4b370fceca502fc.2592000.1618729517.282335-23829922"
@@ -29,7 +28,7 @@ func BaiduWebimage(c *gin.Context) {
 	data["image"] = []string{uploadImagetoBase64}
 	data["detect_direction"] = []string{"true"}
 	//把post表单发送给目标服务器
-	resp, err := http.PostForm("https://aip.baidubce.com/rest/2.0/ocr/v1/webimage?access_token=24.2f5612a038f5c41bf4b370fceca502fc.2592000.1618729517.282335-23829922", data)
+	resp, err := http.PostForm("https://aip.baidubce.com/rest/2.0/ocr/v1/webimage?access_token=24.63b996af3629d9380f346fb432d7ba79.2592000.1621492446.282335-23829922", data)
 	if err != nil {
 		fmt.Println(err.Error())
 		c.JSON(http.StatusOK, gin.H{
@@ -37,6 +36,7 @@ func BaiduWebimage(c *gin.Context) {
 		})
 		return
 	}
+	//关闭请求
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -57,6 +57,7 @@ func BaiduWebimage(c *gin.Context) {
 		txt = txt + txtLine.Words + "\r\n"
 	}
 	fmt.Println(txt)
+	//创建txt
 	f, err := os.Create(__dirname + fileName + ".txt")
 	defer f.Close()
 	if err != nil {
@@ -66,7 +67,7 @@ func BaiduWebimage(c *gin.Context) {
 
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"message": webimageResult.Words_result,
+		"message": webimageResult,
 	})
 
 }
